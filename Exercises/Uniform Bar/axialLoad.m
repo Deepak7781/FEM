d48e37e88d7e8688832a4d9ca217c35109b7d7f2
Deepak7781 <deepak21082005@gmail.com>
2026-05-12 22:10:58 +0530

    fix codes

--- Exercises/Uniform Bar/axialLoad.m
+++ Exercises/Uniform Bar/axialLoad.m
@@ -4,7 +4,44 @@
 
 L = 2; % Length of the bar in meters
 A = 1; % Cross-sectional area in square meters
-E = 200e9; % Young
+E = 200e9; % Young's modulus in Pascals
+N = 2; % Number of elements
+q = 1000; % Axial load in N/m
+
+Le = L/N;
+
+% Element Stiffness Matrix
+ke = A*E/Le;
+
+% Element load vector
+Fe = (q*Le/2)*[1;1];
+
+K = zeros(N+1); % Global stiffness matrix
+F = zeros(N+1, 1); % Global force vector
+
+for i = 1:N
+    K(i:i+1, i:i+1) = K(i:i+1, i:i+1) + ke*[1 -1; -1 1];
+    F(i:i+1) = F(i:i+1) + Fe; 
+end
+
+% Apply Boundary condition
+u1 = 0;
+
+U_reduced = K(2:end, 2:end)\F(2:end);
+
+U = [u1;U_reduced];
+
+% Calculate the stresses in each element
+stress = zeros(N, 1);
+for i = 1:N
+    stress(i) = (U(i+1) - U(i)) * (E / Le);
+end
+
+% Display the nodal displacements and stresses
+disp('Nodal Displacements (m):');
+disp(U);
+disp('Element Stresses (Pa):');
+disp(stress);
 
 
 
